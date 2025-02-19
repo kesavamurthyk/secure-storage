de9aa09224bc66bcb2eef4672a96efa4e5fe39cf
kesavamurthyk <kesavan.k2014@gmail.com>
2025-02-19 14:40:36 +0530

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,87 @@
+mod utils;
+mod xchacha20poly1305;
+extern crate web_sys;
+
+use std::sync::Mutex;
+use wasm_bindgen::prelude::*;
+use web_sys::{window, Storage, console};
+use xchacha20poly1305::XChaCha20Poly1305Wrapper;
+// use lazy_static::lazy_static;
+// use serde_json::{Value, json};
+
+// lazy_static! {
+//     static ref CONFIG_VALUE: Mutex<Value> = Mutex::new(json!({})); // Default empty JSON object
+// }
+
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+fn get_storage() -> Storage {
+    window()
+        .expect("window not found")
+        .local_storage()
+        .expect("Failed to access localStorage")
+        .expect("localStorage not detected")
+}
+
+#[wasm_bindgen]
+pub fn set_item(key: &str, data: &str) {
+    let storage = get_storage();
+    let chacha20 = XChaCha20Poly1305Wrapper::new();
+    let cipher_text = chacha20.encrypt_data(data.as_bytes()).expect("Encryption failed");
+    storage.set_item(key, &cipher_text).expect("Failed to store encrypted data");
+}
+
+#[wasm_bindgen]
+pub fn get_item(key: &str) -> String {
+    let storage = get_storage();
+    let chacha20 = XChaCha20Poly1305Wrapper::new();
+
+    if let Some(data) = storage.get_item(key).expect("Failed to retrieve data") {
+        return match chacha20.decrypt_data(&data) {
+            Ok(decrypted_data) => decrypted_data,
+            Err(e) => {
+                console::error_1(&JsValue::from_str(e));
+                return "".to_string();
+            }
+        }
+    }
+    "".to_string()
+}
+
+#[wasm_bindgen]
+pub fn remove_item(key: &str) {
+    let storage = get_storage();
+
+    storage.remove_item(key).expect("Failed to remove data");
+
+}
+
+#[wasm_bindgen]
+pub fn clear() {
+    let storage = get_storage();
+    storage.clear().expect("Failed to clear data");
+}
+
+// #[wasm_bindgen]
+// pub fn set_config(json_str: String) {
+//     match serde_json::from_str::<Value>(&json_str) {
+//         Ok(parsed_json) => {
+//             let mut config = CONFIG_VALUE.lock().unwrap();
+//             *config = parsed_json;
+//         }
+//         Err(_) => console::error_1(&JsValue::from("Invalid configuration")),
+//     }
+// }
+//
+// #[wasm_bindgen]
+// pub fn get_config() -> String {
+//     let config = CONFIG_VALUE.lock().unwrap();
+//     serde_json::to_string(&*config).unwrap()
+// }
---
+++ src/xchacha20poly1305.rs
@@ -0,0 +1,67 @@
+use std::convert::TryInto;
+use chacha20poly1305::{aead::{Aead, KeyInit, OsRng}, AeadCore, XChaCha20Poly1305, XNonce};
+use base64::{engine::general_purpose, Engine as _};
+use chacha20poly1305::aead::generic_array::GenericArray;
+use chacha20poly1305::aead::rand_core::{RngCore};
+use wasm_bindgen::JsValue;
+
+extern crate web_sys;
+
+pub struct XChaCha20Poly1305Wrapper {
+    encoder: general_purpose::GeneralPurpose,
+    key: [u8; 32],
+}
+
+impl XChaCha20Poly1305Wrapper {
+    pub fn new() -> Self {
+        let mut key = [0u8; 32];
+        OsRng.fill_bytes(&mut key);
+        Self {
+            key,
+            encoder: general_purpose::STANDARD,
+        }
+    }
+
+    fn get_nonce(&self) -> XNonce {
+        XChaCha20Poly1305::generate_nonce(&mut OsRng)
+    }
+
+    fn vec_to_string(data: Vec<u8>) -> String {
+        data.iter().map(|&x| x as char).collect::<String>()
+    }
+
+    pub fn encrypt_data(&self, data: &[u8]) -> Result<String, &'static str> {
+        let cipher = XChaCha20Poly1305::new(&GenericArray::from(self.key));
+        let gen_nonce = Self::get_nonce(&self);
+        let nonce = XNonce::from_slice(&gen_nonce);
+
+        let encrypted_data = cipher.encrypt(&nonce, data).map_err(|_| "Encryption failed")?;
+        let encoded_data = self.encoder.encode::<&Vec<u8>>(&encrypted_data.as_ref());
+        let encoded_nonce = self.encoder.encode::<&[u8]>(nonce);
+        let encoded_key = self.encoder.encode::<&[u8; 32]>(&self.key);
+
+        Ok(format!("{encoded_data}_{encoded_nonce}_{encoded_key}"))
+    }
+
+    pub fn decrypt_data(&self, encrypted: &str) -> Result<String, &'static str> {
+        let parts: Vec<&str> = encrypted.split('_').collect();
+        if parts.len() != 3 {
+            use web_sys::console;
+            console::error_1(&JsValue::from_str(&"Invalid Encryption"));
+            return Ok("".to_string());
+        }
+
+        let mut encrypted_data = self.encoder.decode(parts[0]).map_err(|_| "Decoding failed")?;
+        let nonce_bytes = self.encoder.decode(parts[1]).map_err(|_| "Decoding nonce failed")?;
+        let key_bytes = self.encoder.decode(parts[2]).map_err(|_| "Decoding key_bytes failed")?;
+        let arr: [u8; 32] = key_bytes.try_into().map_err(|_| "Invalid key")?;
+
+        let cipher = XChaCha20Poly1305::new(&GenericArray::from(arr));
+
+        let nonce = XNonce::from_slice(&nonce_bytes);
+
+        let decrypted_data = cipher.decrypt(nonce, encrypted_data.as_ref()).map_err(|_| "Decoding failed")?;
+
+        Ok(decrypted_data.into_iter().map(|b| b as char).collect())
+    }
+}
